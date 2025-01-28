6ad4c0074df27a9a2f99ffaa513862172dd02d6e
Eder Moura <88554733+Drake-Montgomery@users.noreply.github.com>
2025-01-28 23:54:12 +0000

    First commit, Hello World

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,6 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
+    printf("Hello World! \n");
     return 0;
 }
